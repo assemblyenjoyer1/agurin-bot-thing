4357445179516b9fbfaa98d218b6f2a5a4f88d40
isjxn <jani123123123.diederich@gmx.de>
2024-03-24 18:30:02 +0100

    first commit

---
+++ src/App.ts
@@ -0,0 +1,8 @@
+export default class App {
+    constructor() {
+    }
+
+    init() {
+        console.log("[App] Init..");
+    }
+}
---
+++ src/providers/TwitchProvider.ts
@@ -0,0 +1,16 @@
+export default class TwitchProvider {
+    constructor() {
+    }
+
+    init() {
+        console.log("[TwitchProvider] Init..");
+    }
+
+    private registerEvents() {
+        console.log("[TwitchProvider] Registering events..");
+    }
+    
+    private registerCommands() {
+        console.log("[TwitchProvider] Registering commands..");
+    }
+}
